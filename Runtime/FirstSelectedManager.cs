d9ed0b6ec43dc20451e3f7d2b682352ca536b06f
Janek <jasiojfd@gmail.com>
2024-09-08 22:20:01 +0200

    namespaces

--- Runtime/FirstSelectedManager.cs
+++ Runtime/FirstSelectedManager.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using SingletonSystem.Runtime;
+using NoReleaseDate.SingletonSystem.Runtime;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
